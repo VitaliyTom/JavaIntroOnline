093da2ec8f0a0fd3ac46a0aa691afdb3ae35cdc1
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-01-13 15:39:28 +0300

    feat: task one complete

--- src/vitaliy/tomashevich/moduleTwo/One_dimensional_arrays/TaskOne.java
+++ src/vitaliy/tomashevich/moduleTwo/One_dimensional_arrays/TaskOne.java
@@ -3,7 +3,48 @@ package vitaliy.tomashevich.moduleTwo.One_dimensional_arrays;
 //      В массив A [N] занесены натуральные числа.
 //      Найти сумму тех элементов, которые кратны данному К.
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TaskOne {
+    public static void main(String[] args) {
 
+        int n;
+        int k;
+        int sum = 0;
+        int[] mass;
 
+        Scanner scr = new Scanner(System.in);
+        Random rnd = new Random();
+
+        do {
+            System.out.println("Введите натуральное число n : ");
+            while (!scr.hasNextInt()) {
+                scr.next();
+                System.out.println("Введите натуральное число n : ");
+            }
+            n = scr.nextInt();
+        } while (n < 0 || n > 100);
+
+        do {
+            System.out.println("Введите натуральное число k : ");
+            while (!scr.hasNextInt()) {
+                scr.next();
+                System.out.println("Введите натуральное число k : ");
+            }
+            k = scr.nextInt();
+        } while (k < 0 || k > 100);
+
+        mass = new int[n];
+
+        for (int i = 0; i < mass.length; i++) {
+
+            mass[i] = rnd.nextInt(100);
+            System.out.println("mass[" + i + "] = " + mass[i]);
+            if (mass[i] % k == 0) {
+                sum = sum + mass[i];
+            }
+        }
+        System.out.println("\n" + "Сумма элементов кратных " + k + " = " + sum);
+    }
 }
