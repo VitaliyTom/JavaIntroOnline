ffda9ef850277c5b2dd20596ed5ce3cb1f0d7ec5
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-01-13 11:01:50 +0300

    add: class TaskOne in module two

---
+++ src/vitaliy/tomashevich/moduleTwo/One_dimensional_arrays/TaskOne.java
@@ -0,0 +1,9 @@
+package vitaliy.tomashevich.moduleTwo.One_dimensional_arrays;
+
+//      В массив A [N] занесены натуральные числа.
+//      Найти сумму тех элементов, которые кратны данному К.
+
+public class TaskOne {
+
+
+}
