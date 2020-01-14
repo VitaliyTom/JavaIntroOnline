97234e97a30f96c0bbb78d290691d8fc37145eee
VitaliyTom <tomashevich.vitaliy88@gmail.com>
2020-01-14 11:38:43 +0300

    feat: task two complete

---
+++ src/vitaliy/tomashevich/moduleTwo/One_dimensional_arrays/TaskTwo.java
@@ -0,0 +1,63 @@
+package vitaliy.tomashevich.moduleTwo.One_dimensional_arrays;
+
+//Дана последовательность действительных чисел а1 ,а2 ,..., аn.
+// Заменить все ее члены, большие данного Z, этим
+//        числом. Подсчитать количество замен.
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TaskTwo {
+
+    public static void main(String[] args) {
+
+        int z;
+        int n;
+        int replacement = 0;
+        int[] mass;
+
+        Random rnd = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        n = rnd.nextInt(25);
+
+        do {
+            System.out.println("Введите число Z : ");
+
+            while (!sc.hasNextInt()) {
+                sc.next();
+                System.out.println("Введите число Z : ");
+            }
+
+            z = sc.nextInt();
+
+        } while (z < 0 || z > 25);
+
+        mass = new int[n];
+
+        System.out.println("N = " + n);
+
+        for (int i = 0; i < mass.length; i++) {
+
+            mass[i] = rnd.nextInt(25);
+
+            System.out.print(mass[i] + ". ");
+
+            if (mass[i] > z) {
+
+                mass[i] = z;
+                replacement++;
+
+            }
+        }
+        System.out.println(" ");
+
+        for (int i = 0; i < mass.length; i++) {
+
+            System.out.print(mass[i] + ". ");
+
+        }
+        System.out.println("\n" + "Количество замен = " + replacement);
+    }
+}
